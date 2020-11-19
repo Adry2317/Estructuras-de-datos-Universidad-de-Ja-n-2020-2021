92a209a1afa1b41133994411c15446a96db62465
Adrián Arboledas Fernandez <aaf00022@red.ujaen.es>
2020-11-19 16:15:41 +0100

    Borrando prac 1

--- practica 1/build/Debug/Cygwin-Windows/Palabra.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/Cygwin-Windows/Palabra.o: Palabra.cpp Palabra.h VDinamico.h
-Palabra.h:
-VDinamico.h:
--- practica 1/build/Debug/Cygwin-Windows/ParPalabras.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/Cygwin-Windows/ParPalabras.o: ParPalabras.cpp ParPalabras.h \
- Palabra.h VDinamico.h
-ParPalabras.h:
-Palabra.h:
-VDinamico.h:
