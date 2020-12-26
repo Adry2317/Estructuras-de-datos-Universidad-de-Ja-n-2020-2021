4efca7993fabfc84a7fd099820d8bde6000eb7f7
Adrián Arboledas Fernandez <aaf00022@red.ujaen.es>
2020-12-26 23:40:18 +0100

    Eliminando Practica5 de develop

--- Practica5/build/Debug/Cygwin-Windows/DiccionarioConVerbos.o.d
+++
@@ -1,7 +0,0 @@
-build/Debug/Cygwin-Windows/DiccionarioConVerbos.o: \
- DiccionarioConVerbos.cpp DiccionarioConVerbos.h Palabra.h Documento.h \
- THashPalabra.h
-DiccionarioConVerbos.h:
-Palabra.h:
-Documento.h:
-THashPalabra.h:
